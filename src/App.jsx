00b84b634c5b9e1cefc1fe23950a35c948e94d52
PINKU RATHOUR <95443990+Npkrathour@users.noreply.github.com>
2023-11-25 00:42:01 +0530

    path changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import Modal01 from "./pages/Modal01";
 import NoPage from "./pages/NoPage";
 import Home from "./pages/Home";
 
-
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route index element={<Home />} />
-          <Route path="/modal" element={<Modal01 />} />
-          <Route path="*" element={<NoPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/modal" element={<Modal01 />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
