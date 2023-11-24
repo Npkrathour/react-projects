b667fac6c5e5b3b73e9f56916e21dc5385d02325
PINKU RATHOUR <95443990+Npkrathour@users.noreply.github.com>
2023-11-25 02:14:17 +0530

    css part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Modal from "./pages/Modal";
 import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/modal" element={<Modal />} />
           <Route path="*" element={<NoPage />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Modal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import TickImg from "../assets/tick.png";
+import { UilTimes } from "@iconscout/react-unicons";
+
+const Modal = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <button className="bg-red-500 rounded-lg text-lg font-bold px-5 py-3 text-white">View Modal</button>
+      </div>
+
+      <div className="bg-[rgba(0,0,0,0.5)] fixed top-0 bottom-0 left-0 right-0">
+        <div className="absolute top-1/2 left-1/2 bg-white p-10 -translate-y-1/2 -translate-x-1/2 w-full max-w-[600px] rounded-2xl">
+          <UilTimes
+            size={45}
+            className="absolute right-5 top-5 cursor-pointer hover:bg-slate-200 rounded-lg p-1 transition ease-in delay-150"
+          />
+          <div className="flex justify-center mb-5">
+            <img src={TickImg} alt="" className="w-[100px]" />
+          </div>
+          <h1 className="text-center text-3xl mb-3 font-bold">Welcome user</h1>
+          <p className="text-lg font-semibold text-gray-500 text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, repudiandae voluptates similique doloremque
+            iste autem nobis sed consequuntur.
+          </p>
+          <div className="text-center mt-5">
+            <button className="bg-green-500 px-12 py-3 rounded-lg font-bold text-lg text-white">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
